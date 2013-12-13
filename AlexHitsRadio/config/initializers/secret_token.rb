ad4b00ed40a8016a6476f9e49b876f7d7ee657d9
Helal Ismail <helal@surveypocket.com>
2013-12-13 11:52:52 +0200

    13-12

---
+++ AlexHitsRadio/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlexHitsRadio::Application.config.secret_token = '9e0ac9cadf3ac35ee6c6075682e83d68024ebf5de227e050c8f9775d3abb8bde1f3918a90abe9288b5d281394aba2dea26af85e01a1e9284dd620a2adc2a324d'
---
+++ AlexHitsRadio/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AlexHitsRadio::Application.config.session_store :cookie_store, key: '_AlexHitsRadio_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AlexHitsRadio::Application.config.session_store :active_record_store
